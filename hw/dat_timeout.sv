d07fe0d0e82b8c668f1e769dfeee6f66c7dbdb67
Axel Vanoni <axvanoni@student.ethz.ch>
2025-12-30 12:48:40 +0100

    Expand logic in dat_timeout
    This is a preparatory step for further changes

--- hw/dat_timeout.sv
+++ hw/dat_timeout.sv
@@ -8,19 +8,28 @@
 `include "common_cells/registers.svh"
 
 module dat_timeout #(
-  parameter int TIMEOUT_COUNTER_WIDTH = 28 // maximum timeout is 2**27
 ) (
   input  logic clk_i,
   input  logic rst_ni,
 
-  input  logic       running_i,
+  input  logic       running_i, // High as long as we are waiting. Pulling low resets the counter
   input  logic [3:0] timeout_bits_i,
 
   output logic       timeout_o
 );
-  logic [TIMEOUT_COUNTER_WIDTH-1:0] timeout_counter_q, timeout_counter_d;
+  // maximum timeout is 2**27
+  logic [27:0] timeout_counter_q, timeout_counter_d;
   `FF (timeout_counter_q, timeout_counter_d, 0, clk_i, rst_ni);
   
+  always_comb begin
+    timeout_counter_d = timeout_counter_q;
+    // reset on low running_i
+    if (!running_i) begin
+      timeout_counter_d = '0;
+    end
+    if (!timeout_o) begin
+      timeout_counter_d = timeout_counter_q + 1;
+    end
+  end
   assign timeout_o = (timeout_counter_q >= (1 << (timeout_bits_i + 13)));
-  assign timeout_counter_d = running_i && !timeout_o ? timeout_counter_q + 1 : 0;
 endmodule
